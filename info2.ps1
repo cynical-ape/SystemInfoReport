ff1f09133e18d3d8a93b505eab6a3a6face3fff8
cynical-ape <is.muhammad.sb@gmail.com>
2026-02-11 22:40:27 +0200

    Add files via upload

---
+++ info2.ps1
@@ -0,0 +1,30 @@
+# 1. Get the data
+$os = Get-CimInstance Win32_OperatingSystem
+$cpu = Get-CimInstance Win32_Processor
+$disk = Get-CimInstance Win32_LogicalDisk | Where-Object DeviceID -eq 'C:'
+
+# 5. $Report
+
+$Report =  "==============================`n" 
+$Report += "        SYSTEM REPORT         `n" 
+$Report += "==============================`n" 
+
+
+$Report+= "Generated on   :   $(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')`n" 
+$Report+= "Computer Name  :   $env:COMPUTERNAME `n"
+$Report+= "User Name      :   $env:USERNAME `n"
+$Report+= "OS Info        :   $($os.Caption) `n"
+$Report+= "------------------------------`n" 
+
+
+$Report+= "CPU Load       :  $($cpu.LoadPercentage)%`n" 
+$Report+= "Total Memory   :  {0:N2} GB`n" -f ($os.TotalVisibleMemorySize / 1mb)
+$Report+= "Free Memory    :  {0:N2} GB`n" -f ($os.FreePhysicalMemory / 1mb)
+$Report+= "Disk Total     :  {0:N2} GB`n" -f ($disk.Size / 1gb)
+$Report+= "Disk Free      :  {0:N2} GB`n" -f ($disk.FreeSpace / 1gb)
+
+$Report+= "==============================`n" 
+
+$Report | Out-File C:\Users\NewUserName\Desktop\SystemInfoReport.txt
+
+
